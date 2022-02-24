4690cf0c74032c163820486277b17920db230ff0
Lawrie Davidson <lawrie.davidson91@gmail.com>
2022-02-24 20:56:41 +0000

    added event

--- information/urls.py
+++ information/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = ('information_home'))
+    path('', views.home, name ='information_home'),
+    path('django_meetup', views.django_meetup, name = 'meetup')
 ]
