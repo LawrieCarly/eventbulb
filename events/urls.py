79567ea3b72e65b900948dd7628de0391c6f2033
Lawrie Davidson <lawrie.davidson91@gmail.com>
2022-03-10 21:05:03 +0000

    started dashboard

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,12 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('list/', views.list, name="events_list"),
-    path('<int:id>/', views.details, name="details_page")
+    path('', views.list, name="events_list"),
+    path('<int:id>/', views.details, name="details_page"),
+    path('<int:id>/add_attending/',
+         views.add_attending, name="events_add_attending"),
+    path('<int:id>/remove_attending/',
+         views.remove_attending, name="events_remove_attending"),
+    path('<int:id>/dashboard/', views.dashboard, name="event_dashboard"),
 
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 from .models import Event
+
+@login_required
+def add_attending(request, id):
+    event = get_object_or_404(Event, id=id)
+    if request.method == "POST":
+        request.user.profile.attending.add(event)
+    return redirect("events_list")
+
+
+@login_required
+def remove_attending(request, id):
+    event = get_object_or_404(Event, id=id)
+    if request.method == "POST":
+        request.user.profile.attending.remove(event)
+    return redirect("events_list")
+
+def dashboard(request,id):
+    event = get_object_or_404(Event,id=id)
+    return render(request, 'events/dashboard.html', {'event': event})
+
 
 def details(request, id):
     eventFromDatabase = get_object_or_404(Event, id = id)
@@ -24,4 +46,4 @@ def list(request):
 
     # eventsList = Event.objects.filter(
     #     datetime__gte=today).filter(**filters).order_by('datetime')
-    # return render(request, 'events/list.html', {'events': events})
+    # return render(request, 'events/list.html', {'events': eventsList})
